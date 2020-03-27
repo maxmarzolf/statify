2b5d4cc24c71d4ac82495e29f7f2416a3188bb10
maxmarzolf <max@treesfromspace.com>
2020-03-26 20:01:02 -0400

    ripped from https://blog.miguelgrinberg.com/post/oauth-authentication-with-flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 import os
 
 app = Flask(__name__)
@@ -6,10 +6,43 @@ client_id = os.environ['client_id']
 
 
 @app.route('/')
-def hello_world():
+def initialize():
     return redirect('https://accounts.spotify.com/en/authorize?'
                     'client_id=' + client_id +
                     '&response_type=token&redirect_uri=https:%2F%2Fgoogle.com')
+
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self, provider_name):
+        self.provider_name = provider_name
+        credentials = app.config[client_id][provider_name]
+        self.consumer_id = credentials['id']
+        self.consumer_secret = credentials['secret']
+
+    def authorize(self):
+        pass
+
+    def callback(self):
+        pass
+
+    def get_callback_url(self):
+        return url_for('oauth_callback', provider=self.provider_name,
+                       _external=True)
+
+    @classmethod
+    def get_provider(self, provider_name):
+        if self.providers is None:
+            self.providers = {}
+            for provider_class in self.__subclasses__():
+                provider = provider_class()
+                self.providers[provider.provider_name] = provider
+        return self.providers[provider_name]
+
+
+class SpotifySignIn(OAuthSignIn):
+    pass
 
 
 if __name__ == '__main__':
